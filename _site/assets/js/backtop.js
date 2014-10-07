9bfd62f108eb75dc08b6131d32b259cb62d9620d
Huansheng MO <xtcmhs@gmail.com>
2014-10-07 17:18:36 +0800

    delete _site.

--- _site/assets/js/backtop.js
+++
@@ -1,4 +0,0 @@
-function toTopHide(){
-    document.documentElement.scrollTop+document.body.scrollTop > 1000 ? document.getElementById("toTop").style.display="block" : document.getElementById("toTop").style.display="none";
-}
-window.onscroll=toTopHide;
